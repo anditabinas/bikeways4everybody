bdba43069a661e4ffb59a4b77face0a07f883817
anditabinas <38903439+anditabinas@users.noreply.github.com>
2019-07-11 23:53:24 +0800

    Update userconfig.js

--- js/userconfig.js
+++ js/userconfig.js
@@ -13,7 +13,7 @@ var config = {
 	mapboxAccessToken :              
             'pk.eyJ1IjoicmVtb3RlZ2VudHJpZnkiLCJhIjoiY2lnanJzMjJpMDA1dnYxbHo5MTZtdGZsYSJ9.gLE8d40zmDAtMSSZyd2h1Q',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
-	cartoDBusername : 'raphaeld',
-	cartoDBinsertfunction : 'insert_bikeways_data',
+	cartoDBusername : 'anditabinas',
+	cartoDBinsertfunction : 'crowdsourcing_p',
 	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://bostoncyclistsunion.org/' target='_blank'>Boston Cyclists Union</a> on biking in Boston.</p><p>It also allows you to submit input on where biking infrastructure can be improved by drawing on the map! The map is restricted to the inner core of Metro Boston, where we concentrate our advocacy.</p>"
 };
